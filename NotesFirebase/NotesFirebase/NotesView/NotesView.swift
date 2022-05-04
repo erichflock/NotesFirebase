64734c3b3fab0f1f69470b145152a3732c8af89c
Erich Flock <erich.flock@check24.de>
2022-05-04 16:21:27 +0200

    Add add button to notes view

--- NotesFirebase/NotesFirebase/NotesView/NotesView.swift
+++ NotesFirebase/NotesFirebase/NotesView/NotesView.swift
@@ -9,12 +9,31 @@ import SwiftUI
 
 struct NotesView: View {
     var body: some View {
-        List {
-            Text("Note 1")
-            Text("Note 2")
-            Text("Note 3")
-            Text("Note 4")
+        VStack {
+            List {
+                Text("Note 1")
+                Text("Note 2")
+                Text("Note 3")
+                Text("Note 4")
+            }
+            
+            Spacer()
+            
+            Button {
+                print("did tap add")
+            } label: {
+                Image(systemName: "plus")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                    .padding()
+            }
+            .background(.white)
+            .foregroundColor(.blue)
+            .controlSize(.large)
+            .cornerRadius(100/2)
+            .padding()
         }
+        .background(Color(.systemGroupedBackground))
     }
 }
 
