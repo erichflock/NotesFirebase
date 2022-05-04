356ecd2cb1d01c5403f864f6c5c86d556f95638e
Erich Flock <erich.flock@check24.de>
2022-05-04 14:10:21 +0200

    Create xcode project

---
+++ NotesFirebase/NotesFirebase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NotesFirebase
+//
+//  Created by Erich Flock on 04.05.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NotesFirebase/NotesFirebase/NotesFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesFirebaseApp.swift
+//  NotesFirebase
+//
+//  Created by Erich Flock on 04.05.22.
+//
+
+import SwiftUI
+
+@main
+struct NotesFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
